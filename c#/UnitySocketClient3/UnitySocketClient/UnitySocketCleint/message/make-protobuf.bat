1a3f55b7b6eb5697d59d3087ab4d3f3b4c431ea7
kingstop <kingstop@sina.com>
2013-09-09 11:45:07 +0800

    delete c# test client

--- c#/UnitySocketClient3/UnitySocketClient/UnitySocketCleint/message/make-protobuf.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set tool=..\3Party\protobuf-net\net
-
-rem ===============================================
-rem  Support
-set proto=login.proto
-%tool%\protogen.exe -i:%proto% -o:%proto%.cs -q
-
-pause
